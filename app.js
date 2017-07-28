ecf735d999c108edf8aa7d3fd7c1b44051de85f3
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-07-28 14:39:59 -0400

    infrastructure built

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
 const path = require('path');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const promisify = require('es6-promisify');
 const expressValidator = require('express-validator');
+const routes = require('./routes/index');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(passport.initialize());
-app.use(passport.session());
+
+app.use(session({
+  secret: process.env.SECRET,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
 app.use((req, res, next) => {
-  req.login = promisify(req.login, req);
+  res.locals.user = req.user || null;
+  res.locals.currentPath = req.path;
+  next();
 });
 
+app.use('/', routes);
+
+// app.use(errorHandlers.notFound);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  // app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+// app.use(errorHandlers.productionErrors);
 module.exports = app;
